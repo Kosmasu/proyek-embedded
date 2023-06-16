be6234c45e98e1afdca99fbc886ee420d5e49e52
Kosmasu <kennylink2626@gmail.com>
2023-06-16 21:44:26 +0700

    pwm kurang satu, butuh info ko tony

--- index.py
+++ index.py
@@ -6,32 +6,34 @@ def main():
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(pins.PIN_MOTOR_A_BACKWARD, GPIO.OUT, initial=GPIO.LOW)
   GPIO.setup(pins.PIN_MOTOR_A_FORWARD, GPIO.OUT, initial=GPIO.LOW)
-  GPIO.setup(pins.PIN_MOTOR_A_PWM, GPIO.OUT, initial=GPIO.HIGH)
   GPIO.setup(pins.PIN_MOTOR_B_BACKWARD, GPIO.OUT, initial=GPIO.LOW)
   GPIO.setup(pins.PIN_MOTOR_B_FORWARD, GPIO.OUT, initial=GPIO.LOW)
-  GPIO.setup(pins.PIN_MOTOR_B_PWM, GPIO.OUT, initial=GPIO.HIGH)
-  pwmA = GPIO.PWM(pins.PIN_MOTOR_A_PWM, 100)
-  pwmA.start(0)
-  pwmB = GPIO.PWM(pins.PIN_MOTOR_B_PWM, 100)
-  pwmB.start(0)
+  pwmA = GPIO.PWM(pins.PIN_MOTOR_A_PWM, 500)
+  pwmA.start(100)
+  pwmB = GPIO.PWM(pins.PIN_MOTOR_B_PWM, 500)
+  pwmB.start(100)
   GPIO.output(pins.PIN_MOTOR_A_FORWARD, GPIO.HIGH)
+  GPIO.output(pins.PIN_MOTOR_B_FORWARD, GPIO.HIGH)
 
   ctr = 0
   try:
     while True:
       print("speed 1")
       pwmA.ChangeDutyCycle(1)
+      pwmB.ChangeDutyCycle(100)
       time.sleep(2)
       print("speed 100")
       pwmA.ChangeDutyCycle(100)
+      pwmB.ChangeDutyCycle(1)
       time.sleep(2)
       ctr += 1
       print("beep ", ctr)
   finally:
     GPIO.output(pins.PIN_MOTOR_A_FORWARD, GPIO.LOW)
-    GPIO.cleanup()
+    GPIO.output(pins.PIN_MOTOR_B_FORWARD, GPIO.LOW)
     pwmA.stop()
     pwmB.stop()
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
